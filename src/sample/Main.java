c29406d301132d25c64b51df73949d04bb8d17db
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-18 18:03:23 +0200

    change screen size

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 700, 750));
+        primaryStage.setTitle("Calendar");
+        primaryStage.setScene(new Scene(root, 560, 750));
         primaryStage.show();
     }
 
