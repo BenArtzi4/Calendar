f33ee84a9467449aebcc66d09038c0a184b9e879
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-18 11:40:43 +0200

    add month list and year list

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -8,9 +10,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller{
@@ -20,8 +24,15 @@ public class Controller{
 
     @FXML
     ListView<String> monthList = new ListView<String>();
-    ObservableList<String> months = FXCollections.observableArrayList (
-            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    //ObservableList<String> months = FXCollections.observableArrayList (
+         //   "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+    String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+
+    @FXML
+    private ListView<Integer> yearList = new ListView<Integer>();
+    ObservableList<Integer> years = FXCollections.observableArrayList();
 
     @FXML
     private Button setMonth;
@@ -29,8 +40,11 @@ public class Controller{
     @FXML
     private Button setYear;
 
-    @FXML
-    private TextField yearText;
+    final int INITIAL_YEAR = 1990;
+    final int FINAL_YEAR = 2050;
+    String currentMonth;
+    int currentYear;
+
 
     @FXML
     void setMonthPressed(ActionEvent event) {
@@ -42,8 +56,26 @@ public class Controller{
 
     }
 
-    public void initialize() {
-        monthList.setItems(months);
+    public void initialize()
+    {
+        monthList.getItems().addAll(months);
+        initializeYearsList();
+        yearList.getItems().addAll(years);
 
+        yearList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
+            @Override
+            public void changed(ObservableValue<? extends Integer> observableValue, Integer integer, Integer t1) {
+                System.out.println("hello");
+            }
+        });
+    }
+
+
+        private void initializeYearsList()
+    {
+        for (int i = INITIAL_YEAR ; i <= FINAL_YEAR ; i++ )
+        {
+            years.add(i);
+        }
     }
 }
